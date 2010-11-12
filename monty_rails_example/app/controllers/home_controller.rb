1b89c84d1f205b0a05b85d6085a4e7f23a57fbf9
Kurt Stephens <ks.github@kurtstephens.com>
2010-11-12 00:17:15 -0600

    Clean up controller.

--- monty_rails_example/app/controllers/home_controller.rb
+++ monty_rails_example/app/controllers/home_controller.rb
@@ -2,14 +2,12 @@ class HomeController < ApplicationController
   def index
     session[:pid] = $$
 
-    text = [ ]
+    @text = text = [ ]
     text << "pid #{$$}"
     text << session[:id].inspect
-    text << request.session.class.inspect
-    text << MontyRailsExample::Application.instance.app.class.inspect
+    #text << request.session.class.inspect
+    #text << MontyRailsExample::Application.instance.app.class.inspect
 
-    $stderr.puts(text * "\n")
-
-    @text = text
+    $stderr.puts("text ==========\n#{text * "\n"}\n=================")
   end
 end
